c8935738c45e617681f27c340c4038a2a30cc4ff
Juan David Figueroa <jdfp2255@gmail.com>
2025-11-27 21:36:47 -0500

    cambio schema instituciones

--- app/schemas/institucion.py
+++ app/schemas/institucion.py
@@ -6,9 +6,9 @@ class InstitucionBase(BaseModel):
 	nombre_institucion: str = Field(min_length=3, max_length=100)
 	direccion: str = Field(min_length=3, max_length=100)
 	id_municipio: int
-
+     
 class RetornarInstitucion(InstitucionBase):
-    cant_convenios: int
+    cant_convenios: int = Field(default=0)
 
 class EditarInstitucion(BaseModel):
     nit_institucion: Optional[str]
